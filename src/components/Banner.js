d35e57c1f36408f9cb7a2d87d6ee00989703bcaa
Quentin CAVIN <quentin.cavin@gmail.com>
2024-05-28 09:41:09 +0200

    Ajout styles components

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,5 +1,13 @@
-function Banner () {
-    return <h1>La maison de la Jungle</h1>
+import "../styles/Banner.css"
+import logo from "../assets/logo.png"
+
+function Banner() {
+  return (
+    <div className="lmj-banner">
+        <img src={logo} alt="la maison jungle" className="lmj-logo" />
+      <h1 className="lmj-tittle">La maison de la Jungle</h1>
+    </div>
+  );
 }
 
-export default Banner
+export default Banner;
