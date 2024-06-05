616162d08fa8ada7b3b3fea1c90001b7f64a6afd
Quentin CAVIN <quentin.cavin@gmail.com>
2024-06-05 17:47:34 +0200

    Création alerte au clique sur CareScale

--- src/components/CareScale.js
+++ src/components/CareScale.js
@@ -21,8 +21,32 @@ function CareScale(props) {
       <img src={water} alt="water-icon" />
     );
 
+  // pour convertir la valeur numérique en texte affiché dans l'alerte
+  let quantityLabel = "";
+  switch (scaleValue) {
+    case 1:
+      quantityLabel = "peu";
+      break;
+    case 2:
+      quantityLabel = "modérément";
+      break;
+    case 3:
+      quantityLabel = "beaucoup";
+      break;
+    default:
+      quantityLabel = "une quantité inconnu";
+  }
+
   return (
-    <div onClick={handleClick}>
+    <div
+      onClick={() =>
+        alert(
+          `cette plante requiert ${quantityLabel} ${
+            careType === "light" ? "de lumière" : "d'arrosage"
+          }`
+        )
+      }
+    >
       {range.map((rangeElem) =>
         scaleValue >= rangeElem ? (
           <span key={rangeElem.toString()}>{scaleType}</span>
@@ -30,11 +54,6 @@ function CareScale(props) {
       )}
     </div>
   );
-}
-
-function handleClick(e) {
-  // Reprendre ICI pour générer une alerte qui dira ""Cette plante requiert peu/modérement/beaucoup de lumière/d'arrosage""
-  alert("alert");
 }
 
 export default CareScale;
